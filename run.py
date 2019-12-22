ead33f52b8db5298ff4105c06bf4408e96428ede
mak <38956972+makarandh@users.noreply.github.com>
2019-12-22 08:55:41 +0000

    changing to postgres on heroku  Changes to be committed: 	modified:   requirements.txt 	modified:   run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
@@ -10,7 +11,7 @@ from sqlalchemy_db import db
 
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "johnsmith"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./database/data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 jwt = JWTManager(app)
 api = Api(app)
